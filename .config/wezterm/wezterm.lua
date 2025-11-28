390854566f123bdbcac0d686c8931d76109b3d66
Eric Xiao <ericxiao0000@gmail.com>
2025-11-27 20:07:42 -0800

    Wezterm background and no padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,15 +17,30 @@ config.background = {
     },
     width = "100%"
   },
+  {
+    source = {
+      Color = "white"
+    },
+    opacity = 0.5,
+    width = "100%",
+    height = "100%",
+  },
 }
 
 config.enable_tab_bar = false
 
 -- or, changing the font size and color scheme.
 config.font = wezterm.font "Iosevka"
-config.font_size = 16
+config.font_size = 18
 config.color_scheme_dirs = { "~/.config/wezterm/colors" }
 config.color_scheme = "Dawnfox"
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 -- config.disable_default_key_bindings = true
 
